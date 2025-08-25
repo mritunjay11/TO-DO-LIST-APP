b83f1da7d05c1742aed20a79c661860c054bec7c
Mritunjay bhardwaj <94426326+mritunjay11@users.noreply.github.com>
2025-08-25 19:26:17 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,44 @@
+function add() {
+    const Input=document.querySelector('#user-input')
+    const ul_list=document.querySelector('.main-list')
+    const value = Input.value
+    
+    if(value !=="")
+        {
+        const li = document.createElement('li')
+        li.classList.add('listitem')
+        const value = Input.value
+        li.innerText=value
+        ul_list.append(li)
+
+        const liRemovebtn = document.createElement('button')
+        liRemovebtn.classList.add('remove-btn')
+        liRemovebtn.innerText="Remove"
+        ul_list.append(liRemovebtn)
+
+
+
+        const donebtn= document.createElement('button')
+        donebtn.classList.toggle('done-btn')
+        donebtn.innerText="Done"
+        ul_list.append(donebtn)
+
+        donebtn.addEventListener("click",(e)=>{
+            donebtn.innerText="Done"
+            li.innerText="Hogya tera task ✅"
+            li.style.fontStyle="italic"
+            donebtn.remove()
+            
+        })
+        
+         liRemovebtn.addEventListener("click",(e)=>{
+            li.remove()
+            liRemovebtn.remove()
+            donebtn.remove()
+        })
+    }
+
+
+}
+
+
